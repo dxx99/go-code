85df9569f92053b09f3c4caa1eff37971b6307b0
dxx99 <dxxxue@gmail.com>
2021-06-15 00:53:13 +0800

    advanced concurrency patterns

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # go-code
 Collecting code related snippets for easy development
 
-## [Concurrency](./concurrency/Readme.md)
-- 
+## concurrency-patterns
+- [Concurrency](./concurrency/Readme.md)
 
---
+++ concurrency/13-adv-pingpong/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Ball struct {
+	hits int
+}
+
+func player(name string, table chan *Ball)  {
+	for {
+		// receive channel, add one , send channel
+		ball := <-table
+		ball.hits++
+		fmt.Printf("%s --> %d\n", name, ball.hits)
+		time.Sleep(time.Millisecond * 1e2)
+		table <- ball
+	}
+}
+
+func main() {
+	table := make(chan *Ball)
+	go player("ping", table)
+	go player("pong", table)
+
+	table <- new(Ball)	// game start; toss the ball
+	time.Sleep(1* time.Second)
+	<-table	//game over; grab the ball
+
+	panic("show me the stacks")
+}
