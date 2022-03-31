611fe74c717c1af176fda9cc828142dc60fa1326
董xx <dxxxue@gmail.com>
2022-04-01 00:12:55 +0800

    get sub max

--- algorithm/leetcode/p53-dynamic-max-sub-array/main.go
+++ algorithm/leetcode/p53-dynamic-max-sub-array/main.go
@@ -1,4 +1,10 @@
 package main
+
+import (
+	"fmt"
+	"math"
+)
+
 // 53. 最大子数组和
 // 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
 //
@@ -31,10 +37,67 @@ package main
 //链接：https://leetcode-cn.com/problems/maximum-subarray
 //著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 func main() {
-
+	nums := []int{-2,1,-3,4,-1,2,1,-5,4}
+	fmt.Println(maxSubArray(nums))
+	fmt.Println(maxSubArrayV3(nums))
 }
 
 //TODO: 实现这个方法
-func maxSubArray(nums int) int {
-	return 0
+// 两次循环, 每一轮中找到最大的
+func maxSubArray(nums []int) int {
+	l := len(nums)
+	max := math.MinInt64
+	for k := 0; k < l; k++ {
+		sum := 0
+		for j := k; j < l; j++ {
+			sum += nums[j]
+			if sum > max {
+				max = sum
+			}
+		}
+	}
+	return max
+}
+
+// 动态规划
+// 如果前一个元素大于零，则将其加到当前元素上
+func maxSubArrayV2(nums []int) int {
+	l := len(nums)
+	if l == 0 {
+		return 0
+	}
+	max := nums[0]
+	for k := 1; k < l; k++ {
+		// 重置当前元素，这样就可以得出最大值的一堆数组了
+		if nums[k] + nums[k-1] > nums[k] {
+			nums[k] = nums[k] + nums[k-1]
+		}
+		if nums[k] > max {
+			max = nums[k]
+		}
+	}
+
+	return max
+}
+
+// 贪心算法
+// 如果当前指针所指元素的和小于0，则丢弃当前元素之前的数列
+func maxSubArrayV3(nums []int) int {
+	l := len(nums)
+	if l == 0 {
+		return 0
+	}
+	max, current := nums[0], nums[0]
+
+	for k := 1; k < l; k++ {
+		if current < 0 {
+			current = 0
+		}
+		current += nums[k]
+		if current > max {
+			max = current
+		}
+	}
+
+	return max
 }
