55beb5272d6993b23e3fe7c9c80194850cc8bc10
董xx <dxxxue@gmail.com>
2022-04-01 01:01:24 +0800

    get sub max

--- algorithm/leetcode/p70-dynamic-climbing-floor/main.go
+++ algorithm/leetcode/p70-dynamic-climbing-floor/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // 70. 爬楼梯
 // 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
 //
@@ -32,10 +34,59 @@ package main
 //链接：https://leetcode-cn.com/problems/climbing-stairs
 //著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 func main() {
-
+	fmt.Println(climbStairsV2(2))
+	fmt.Println(climbStairsV2(3))
+	fmt.Println(climbStairsV2(100))
 }
 
-//TODO: 实现这个防范
+// 递归请求
+// 这个就是类似于斐波拉契队列
+// f(n) = f(n-1) + f(n-2)  假设当前在第n个台阶，则他上一个台阶在n-1或者n-2
 func climbStairs(n int) int {
-	return 0
+	// 临界条件
+	if n == 1 {
+		return 1
+	}
+	if n == 2 {
+		return 2
+	}
+
+	return climbStairs(n-1) + climbStairs(n-2)
+}
+
+// 递归优化
+func climbStairsV2(n int) int {
+	// 临界条件
+	m := make(map[int]int)
+	return helper(n, m)
+}
+
+func helper(n int, m map[int]int) int {
+	if v, ok := m[n]; ok {
+		return v
+	}
+	if n == 1 {
+		m[1] = 1
+	}else if n == 2 {
+		m[2] = 2
+	}else {
+		m[n] = helper(n-1, m) + helper(n-2, m)
+	}
+	return m[n]
+}
+
+
+
+// 使用迭代求解
+func climbStairsV3(n int) int {
+	if n == 1 {
+		return 1
+	}
+
+	// 可以创建一个数组，记录各个结果的值
+	f1, f2 := 1, 2
+	for i := 3; i < n; i++ {
+		f1, f2 = f2, f1+f2
+	}
+	return f2
 }
